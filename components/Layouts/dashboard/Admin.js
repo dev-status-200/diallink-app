54765f0872ac88e4422c77976516e910c79d9151
dev-status-200 <dev.status.200@gmail.com>
2022-12-05 02:25:12 +0500

    asd

--- components/Layouts/dashboard/Admin.js
+++ components/Layouts/dashboard/Admin.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Table } from 'react-bootstrap'
+import moment from 'moment'
 
 const Admin = ({adminCallsData}) => {
-
   const [ calls, setCalls ] = useState({
     complete:0,
     cancel:0,
     ongoing:0,
-  }) 
+  });
+  const [ status, setStatus ] = useState('');
 
   useEffect(() => {
     console.log(adminCallsData)
@@ -19,19 +20,13 @@ const Admin = ({adminCallsData}) => {
       if(x.status==3 && x.cost!=0){
         complete = complete + 1
       }
-
       if(x.status==2){
         ongoing = ongoing + 1
       }
-
       if(x.status==3 && x.cost==0){
         cancel = cancel + 1
       }
-
     });
-    console.log(complete)
-    console.log(cancel)
-    console.log(ongoing)
 
     setCalls({
       complete:complete,
@@ -41,50 +36,30 @@ const Admin = ({adminCallsData}) => {
     
     return () => { };
   }, [])
-
+  
   return (
     <div>
       <div className={''}>
         <Row className='box m-3'>
-          <Col md={3}>
+        <Col md={12}><h2 className='f my-2'>Calls</h2></Col>
+          <Col md={3} onClick={()=>setStatus(3)}>
            <div className='completeCalls'>
             <h5 className='wh'>Completed Calls</h5>
             <h3 className='wh'>{calls.complete}</h3>
            </div> 
           </Col>
-          <Col md={3}>
+          <Col md={3} onClick={()=>setStatus(2)}>
            <div className='ongoingCalls'>
             <h5 className='wh'>Ongoing Calls</h5>
             <h3 className='wh'>{calls.ongoing}</h3>
            </div> 
           </Col>
-          <Col md={3}>
+          <Col md={3} onClick={()=>setStatus(1)}>
            <div className='cancelCalls'>
             <h5 className='wh'>Cancelled Calls</h5>
             <h3 className='wh'>{calls.cancel}</h3>
            </div> 
           </Col>
-          {/* <Col><h3 className='f my-2'>Calls</h3></Col>
-          <Col>
-          <Row style={{textAlign:'right'}}>
-            <Col md={4}></Col>
-            <Col md={4}>
-            <Form.Select aria-label="Default select example" onChange={(e)=>setCallStatus(e.target.value)}>
-              <option value='all'>All Calls</option>
-              <option value="0" style={{color:'#ae1313', fontWeight:500}}>Rejected</option>
-              <option value="1" style={{color:'#bdab47', fontWeight:500}}>Pending</option>
-              <option value="2" style={{color:'#2a58d5', fontWeight:500}}>Approved</option>
-              <option value="3" style={{color:'#1f841a', fontWeight:500}}>Completed</option>
-            </Form.Select>
-            </Col>
-            <Col md={4}> 
-            <button className='custom-btn' onClick={()=>setVisible(true)}>Create New</button>
-            </Col>
-          </Row>
-          </Col>
-          <div className='px-2'>
-          <hr className='my-2' />
-          </div>
           <div className='table-sm-1 mt-3'>
             <Table className='tableFixHead'>
             <thead>
@@ -96,16 +71,15 @@ const Admin = ({adminCallsData}) => {
                 <th>Status</th>
                 <th>Assigned Vendor</th>
                 <th>Created</th>
-                <th>Modify</th>
+                <th>Agent</th>
               </tr>
             </thead>
             <tbody>
-            {
-              callList.filter((x)=>{
-                if(callStatus=='all'){
+            {adminCallsData.filter((x)=>{
+                if(status==''){
                   return x
                 }else{
-                  return x.status==callStatus
+                  return x.status==status
                 }
               }).map((x, index) => {
               return (
@@ -125,15 +99,9 @@ const Admin = ({adminCallsData}) => {
                 <td>{x.Vendor!=null?x.Vendor.f_name:'----'} {x.Vendor!=null?x.Vendor.l_name:''}</td>
                 <td>{moment(x.createdAt).fromNow()}</td>
                 <td>
-                  <span style={{cursor:'pointer'}} onClick={()=>{
-                        if(x.status=='0'){setSelectedCall(x);
-                        setEditVisible(true);}
-                      }}>
-                    <EditOutlined className='modify-edit' />
-                  </span> <span className='mx-1'> | </span>
-                  <span>
-                    <CloseCircleOutlined className='modify-red'/>
-                  </span>
+                  {x.User.f_name}
+                  {" "}
+                  {x.User.l_name}
                 </td>
               </tr>
                 )
@@ -141,7 +109,7 @@ const Admin = ({adminCallsData}) => {
             }
             </tbody>
             </Table>
-          </div> */}
+          </div> 
         </Row>
       </div>
     </div>
